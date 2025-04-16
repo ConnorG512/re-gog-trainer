4a19b8d9794050e90733a63615730d3058b0b526
Connor Garey <connorgarey1999@gmail.com>
2025-04-16 21:29:37 +0100

    Seperated the launcher into multiple functions for easier reading.

--- src/launcher/launcher.cpp
+++ src/launcher/launcher.cpp
@@ -4,15 +4,39 @@
 #include <libloaderapi.h>
 #include <memoryapi.h>
 #include <minwinbase.h>
+#include <process.h>
 #include <processthreadsapi.h>
 #include <windows.h>
 #include <winnt.h>
 
+// Function declarations
+void launchTargetProcess(PROCESS_INFORMATION* process_info);
+LPVOID AllocateMemoryInAddressSpace(PROCESS_INFORMATION* process_info, SIZE_T dll_length);
+void writeToProcessMemory(PROCESS_INFORMATION* process_info, SIZE_T dll_length, LPVOID p_dll_reserved_address);
+FARPROC GetProcessAddress();
+HANDLE CreateRemoteThreadInProcess(PROCESS_INFORMATION* process_info, FARPROC load_library_address, LPVOID p_dll_reserved_address);
+
 int main (int argc, char *argv[]) {
-  STARTUPINFO startup_info {};
+  SIZE_T dll_length = strlen("re-trainer.dll") + 1; // + 1 for null terminator byte
   PROCESS_INFORMATION process_info {};
-  
-  // Create the executeable process
+ 
+  launchTargetProcess(&process_info);
+  LPVOID p_dll_reserved_address = AllocateMemoryInAddressSpace(&process_info, dll_length);
+  writeToProcessMemory(&process_info, dll_length, p_dll_reserved_address);
+  FARPROC load_library_address = GetProcessAddress();
+  HANDLE process_handle = CreateRemoteThreadInProcess(&process_info, load_library_address, p_dll_reserved_address);
+
+  printf("DLL reserved address: %p\n", p_dll_reserved_address);
+  printf("Load Library Address: %p\n", load_library_address);
+  printf("Process Handle: %p\n", process_handle);
+  return 0;
+}
+
+// Function definitions
+void launchTargetProcess(PROCESS_INFORMATION *process_info) 
+{
+  STARTUPINFO startup_info {};
+
   if (CreateProcessA(
         "ResidentEvil.exe",
         nullptr, 
@@ -23,16 +47,18 @@ int main (int argc, char *argv[]) {
         nullptr, 
         nullptr, 
         &startup_info, 
-        &process_info) == FALSE) 
+        process_info) == FALSE)
   {
     MessageBoxA(NULL, "Failed to create process!", "Launcher Error", MB_OK | MB_ICONERROR);
-    return 1;
+    abort();
   }
-  
+}
+
+LPVOID AllocateMemoryInAddressSpace(PROCESS_INFORMATION* process_info, SIZE_T dll_length) 
+{
   // Allocate memory in to the address space of the process
-  SIZE_T dll_length = strlen("re-trainer.dll") + 1; // + 1 for null terminator byte
   auto p_dll_reserved_address = VirtualAllocEx(
-      process_info.hProcess, 
+      process_info -> hProcess, 
       nullptr, 
       dll_length,
       MEM_COMMIT, 
@@ -41,14 +67,15 @@ int main (int argc, char *argv[]) {
   {
     printf("Error! could not get base address: %lu", GetLastError());
     MessageBoxA(NULL, "Failed to get base address!", "Launcher Error", MB_OK | MB_ICONERROR);
-    return 1;
   }
-  
-  printf("DLL reserved address: %p\n", p_dll_reserved_address);
+  return p_dll_reserved_address;
+}
 
+void writeToProcessMemory(PROCESS_INFORMATION* process_info, SIZE_T dll_length, LPVOID p_dll_reserved_address)
+{
   const char* dll_path = "re-trainer.dll";
   if (!WriteProcessMemory(
-        process_info.hProcess, 
+        process_info -> hProcess, 
         p_dll_reserved_address, 
         dll_path, 
         dll_length, 
@@ -56,24 +83,29 @@ int main (int argc, char *argv[]) {
   {
     printf("Error! could not write dll to memroy: %lu\n", GetLastError());
     MessageBoxA(NULL, "Failed to assign data to dll address!", "Launcher Error", MB_OK | MB_ICONERROR);
-    return 1;
   }
+}
 
-  auto p_load_library_address = GetProcAddress(
+FARPROC GetProcessAddress()
+{
+  FARPROC p_load_library_address = GetProcAddress(
       GetModuleHandleA("kernel32.dll"), 
       "LoadLibraryA");
   if (p_load_library_address == nullptr) 
   {
     printf("Error finding LoadLibraryA address! %lu\n", GetLastError());
     MessageBoxA(NULL, "Failed to get LoadLibraryA address!", "Launcher Error", MB_OK | MB_ICONERROR);
-    return 1;
   }
-  
+  return p_load_library_address;
+}
+
+HANDLE CreateRemoteThreadInProcess(PROCESS_INFORMATION* process_info, FARPROC load_library_address, LPVOID p_dll_reserved_address) 
+{
   HANDLE remote_thread = CreateRemoteThread(
-      process_info.hProcess, 
+      process_info -> hProcess, 
       nullptr, 
       0, 
-      (LPTHREAD_START_ROUTINE)p_load_library_address, // Cast otherwise, 'No Matching Function Call' 
+      (LPTHREAD_START_ROUTINE)load_library_address, // Cast otherwise, 'No Matching Function Call' 
       p_dll_reserved_address, 
       0, 
       nullptr);
@@ -81,5 +113,5 @@ int main (int argc, char *argv[]) {
     printf("Error creating remote thread! %lu\n", GetLastError());
     MessageBoxA(NULL, "Failed to create remote thread!", "Launcher Error", MB_OK | MB_ICONERROR);
   }
-  return 0;
-}
+  return remote_thread;
+ }
